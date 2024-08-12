e4101b57d30d5f1d5a70a92163dc6294a2314172
Joohyeji <110455871+Joohyeji@users.noreply.github.com>
2024-08-13 00:37:50 +0900

    Time: 59 ms (42.82%), Space: 52 MB (59.33%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    let low = 0;
+    let high = nums.length - 1
+
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2)
+
+        if (nums[mid] === target) {
+            return mid
+        }
+        else if (target < nums[mid]) {
+            high = mid - 1
+        }
+        else if (target > nums[mid]) {
+            low = mid + 1
+        }
+    }
+        return -1
 };
