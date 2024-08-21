0d688d77fe5188e0f76617f39106697c710ad5ba
Joohyeji <110455871+Joohyeji@users.noreply.github.com>
2024-08-21 15:54:50 +0900

    Time: 154 ms (47.1%), Space: 60.5 MB (44.03%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function (mat) {
+  let rows = mat.length;
+  let cols = mat[0].length;
+  let dist = Array.from({ length: rows }, () => Array(cols).fill(Infinity));
+  let queue = [];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (mat[i][j] === 0) {
+        dist[i][j] = 0;
+        queue.push([i, j]);
+      }
+    }
+  }
+
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  while (queue.length > 0) {
+    const [x,y] = queue.shift();
+
+    for (let [dx, dy] of directions) {
+      let newX = x + dx;
+      let newY = y + dy;
+
+      if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+        if (dist[newX][newY] > dist[x][y] + 1) {
+          dist[newX][newY] = dist[x][y] + 1
+          queue.push([newX, newY])
+        }
+      }
+    }
+  }
+
+  return dist
+};
